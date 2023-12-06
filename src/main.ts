6058cc46f5e008d3edb07b56d6fafe7109f681fb
Trunk <Jesdakorns@gmail.com>
2023-12-07 03:29:13 +0700

    fix main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(),
+  );
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://e-commerce-web-eta.vercel.app'], // Allow requests from your frontend application
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Enable credentials (cookies, authorization headers)
+  });
+  await app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT || 8000);
+  console.log('host: ', await app.getUrl());
 }
 bootstrap();
