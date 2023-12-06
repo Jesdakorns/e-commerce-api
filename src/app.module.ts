0310c3358777e1d55f604594edf9e34975930ee6
Trunk <Jesdakorns@gmail.com>
2023-12-07 02:51:38 +0700

    no message

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,71 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { config as dotenvConfig } from 'dotenv';
-import { CacheModule } from '@nestjs/cache-manager';
-import { MulterModule } from '@nestjs/platform-express';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-// import { AuthModule } from './auth/auth.module';
-// import { UserModule } from './user/user.module';
-import { JwtModule } from '@nestjs/jwt';
-dotenvConfig({ path: '.env' });
+
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(process.cwd(), 'public'),
-      serveRoot: '/public',
-      serveStaticOptions: {
-        extensions: ['jpg', 'jpeg', 'png', 'gif'],
-        index: false,
-      },
-    }),
-    MulterModule.register({
-      dest: './files',
-    }),
-    CacheModule.register({
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT,
-      password: process.env.REDIS_PASS,
-      ttl: 10000 * 60 * 60,
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: `${process.env.DATABASE_HOST}`,
-      port: +process.env.DATABASE_PORT,
-      username: `${process.env.DATABASE_USERNAME}`,
-      password: `${process.env.DATABASE_PASSWORD}`,
-      database: `${process.env.DATABASE_NAME}`,
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      migrations: ['dist/migrations/*{.ts,.js}'],
-      autoLoadEntities: true,
-      synchronize: true,
-      logging: true,
-      ssl:
-        process.env.NODE_ENV === 'production'
-          ? {
-              rejectUnauthorized: false,
-            }
-          : undefined,
-      extra:
-        process.env.NODE_ENV === 'production'
-          ? {
-              sslmode: 'require',
-            }
-          : undefined,
-    }),
-    JwtModule.register({
-      global: true,
-      secret: `${process.env.JWT_KEY}`,
-      signOptions: { expiresIn: '7d' },
-    }),
-    MongooseModule.forRoot(`${process.env.MONGODB_HOST}`),
-    // AuthModule,
-    // UserModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import {
-  ExpressAdapter,
-  NestExpressApplication,
-} from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-    new ExpressAdapter(),
-  );
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://e-commerce-web-eta.vercel.app'], // Allow requests from your frontend application
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Enable credentials (cookies, authorization headers)
-  });
-  await app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 8000);
-  console.log('host: ', await app.getUrl());
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
