2d0f77b913dd339f8341d7665a48a5e8054b460b
Trunk <Jesdakorns@gmail.com>
2023-12-07 04:00:14 +0700

    connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,43 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig({ path: '.env' });
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'public'),
+      serveRoot: '/public',
+      serveStaticOptions: {
+        extensions: ['jpg', 'jpeg', 'png', 'gif'],
+        index: false,
+      },
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: `${process.env.DATABASE_HOST}`,
+      port: +process.env.DATABASE_PORT,
+      username: `${process.env.DATABASE_USERNAME}`,
+      password: `${process.env.DATABASE_PASSWORD}`,
+      database: `${process.env.DATABASE_NAME}`,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/*{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
