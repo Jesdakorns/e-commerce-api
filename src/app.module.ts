a52b3bae688399cbf37fb0255b1cf30b2f7d01b3
Trunk <Jesdakorns@gmail.com>
2023-12-07 04:46:30 +0700

    fix user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config as dotenvConfig } from 'dotenv';
+import { UserModule } from './user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { CacheModule } from '@nestjs/cache-manager';
 
 dotenvConfig({ path: '.env' });
-console.log('__dirname', __dirname + '/../**/*.entity{.ts,.js}');
 @Module({
   imports: [
+    JwtModule.register({
+      global: true,
+      secret: `${process.env.JWT_KEY}`,
+      signOptions: { expiresIn: '7d' },
+    }),
+    CacheModule.register({
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASS,
+      ttl: 10000 * 60 * 60,
+      isGlobal: true,
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), 'public'),
       serveRoot: '/public',
@@ -20,25 +34,26 @@ console.log('__dirname', __dirname + '/../**/*.entity{.ts,.js}');
       },
     }),
     MongooseModule.forRoot(`${process.env.MONGODB_HOST}`),
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: `${process.env.DATABASE_HOST}`,
-    //   port: +process.env.DATABASE_PORT,
-    //   username: `${process.env.DATABASE_USERNAME}`,
-    //   password: `${process.env.DATABASE_PASSWORD}`,
-    //   database: `${process.env.DATABASE_NAME}`,
-    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    //   migrations: ['dist/migrations/*{.ts,.js}'],
-    //   autoLoadEntities: true,
-    //   synchronize: true,
-    //   logging: true,
-    //   ssl: true,
-    //   extra: {
-    //     ssl: {
-    //       rejectUnauthorized: false,
-    //     },
-    //   },
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: `${process.env.DATABASE_HOST}`,
+      port: +process.env.DATABASE_PORT,
+      username: `${process.env.DATABASE_USERNAME}`,
+      password: `${process.env.DATABASE_PASSWORD}`,
+      database: `${process.env.DATABASE_NAME}`,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/*{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
