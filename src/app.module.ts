8a7097b806ccd42c891e8f2294daf12ce980ed95
Trunk <Jesdakorns@gmail.com>
2023-12-07 04:35:49 +0700

    no message

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 import { MongooseModule } from '@nestjs/mongoose';
 import { config as dotenvConfig } from 'dotenv';
 
@@ -20,25 +20,25 @@ console.log('__dirname', __dirname + '/../**/*.entity{.ts,.js}');
       },
     }),
     MongooseModule.forRoot(`${process.env.MONGODB_HOST}`),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: `${process.env.DATABASE_HOST}`,
-      port: +process.env.DATABASE_PORT,
-      username: `${process.env.DATABASE_USERNAME}`,
-      password: `${process.env.DATABASE_PASSWORD}`,
-      database: `${process.env.DATABASE_NAME}`,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      migrations: ['dist/migrations/*{.ts,.js}'],
-      autoLoadEntities: true,
-      synchronize: true,
-      logging: true,
-      ssl: true,
-      extra: {
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: `${process.env.DATABASE_HOST}`,
+    //   port: +process.env.DATABASE_PORT,
+    //   username: `${process.env.DATABASE_USERNAME}`,
+    //   password: `${process.env.DATABASE_PASSWORD}`,
+    //   database: `${process.env.DATABASE_NAME}`,
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   migrations: ['dist/migrations/*{.ts,.js}'],
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    //   logging: true,
+    //   ssl: true,
+    //   extra: {
+    //     ssl: {
+    //       rejectUnauthorized: false,
+    //     },
+    //   },
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
